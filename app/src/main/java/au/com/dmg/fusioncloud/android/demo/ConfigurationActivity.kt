e0eb9312b3f87f685c2dea982081d4671f1ab05d
Vanessa Agustin <vanessa_agustin@yahoo.com>
2023-11-06 09:18:04 +1100

    + ui updates

--- app/src/main/java/au/com/dmg/fusioncloud/android/demo/ConfigurationActivity.kt
+++ app/src/main/java/au/com/dmg/fusioncloud/android/demo/ConfigurationActivity.kt
@@ -2,10 +2,9 @@ package au.com.dmg.fusioncloud.android.demo
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
-import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import au.com.dmg.fusioncloud.android.demo.databinding.ConfigurationActivityBinding
 import au.com.dmg.fusioncloud.android.demo.utils.Configuration.applicationName
 import au.com.dmg.fusioncloud.android.demo.utils.Configuration.certificationCode
 import au.com.dmg.fusioncloud.android.demo.utils.Configuration.kek
@@ -15,80 +14,57 @@ import au.com.dmg.fusioncloud.android.demo.utils.Configuration.saleId
 import au.com.dmg.fusioncloud.android.demo.utils.Configuration.softwareVersion
 
 class ConfigurationActivity : AppCompatActivity() {
-    var txtSalesID: EditText? = null
-    var txtPOIID: EditText? = null
-    var txtKEK: EditText? = null
-    lateinit var txtProviderIdentifiction: EditText
-    lateinit var txtApplicationName: EditText
-    lateinit var txtSoftwareVersion: EditText
-    lateinit var txtCertificationCode: EditText
+    private lateinit var binding: ConfigurationActivityBinding
     var previousClass: Class<*>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.configuration_activity)
+        binding = ConfigurationActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         previousClass = intent.javaClass
-        txtSalesID = findViewById(R.id.txtSalesID)
-        txtPOIID = findViewById(R.id.txtPOIID)
-        txtKEK = findViewById(R.id.txtKEK)
-        txtProviderIdentifiction = findViewById(R.id.txtProviderIdentifiction)
-        txtApplicationName = findViewById(R.id.txtApplicationName)
-        txtSoftwareVersion = findViewById(R.id.txtSoftwareVersion)
-        txtCertificationCode = findViewById(R.id.txtCertificationCode)
-        getvalues()
+
+        getValues()
 
         //disable editing for Static Sales Systems
-        txtProviderIdentifiction.setFocusable(false)
-        txtProviderIdentifiction.setClickable(false)
-        txtApplicationName.setFocusable(false)
-        txtApplicationName.setClickable(false)
-        txtSoftwareVersion.setFocusable(false)
-        txtSoftwareVersion.setClickable(false)
-        txtCertificationCode.setFocusable(false)
-        txtCertificationCode.setClickable(false)
-    }
+        binding.txtSalesID.isFocusable = false
+        binding.txtProviderIdentifiction.isClickable = false
+        binding.txtApplicationName.isFocusable= false
+        binding.txtApplicationName.isClickable= false
+        binding.txtSoftwareVersion.isFocusable= false
+        binding.txtSoftwareVersion.isClickable= false
+        binding.txtCertificationCode.isFocusable= false
+        binding.txtCertificationCode.isClickable= false
 
-    private fun getvalues() {
-        txtSalesID!!.setText(saleId)
-        txtPOIID!!.setText(poiId)
-        txtKEK!!.setText(kek)
-        txtProviderIdentifiction!!.setText(providerIdentification)
-        txtApplicationName!!.setText(applicationName)
-        txtSoftwareVersion!!.setText(softwareVersion)
-        txtCertificationCode!!.setText(certificationCode)
-    }
+        binding.btnExit.setOnClickListener {
+            finish()
+        }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.back, menu)
-        menuInflater.inflate(R.menu.savesettings, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.savesettings -> {
-                savesettings()
-                true
-            }
-
-            R.id.back -> {
-                finish()
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
+        binding.btnSave.setOnClickListener {
+            saveSettings()
         }
     }
 
-    private fun savesettings() {
+    private fun getValues() {
+        binding.txtSalesID.setText(saleId)
+        binding.txtPOIID.setText(poiId)
+        binding.txtKEK.setText(kek)
+        binding.txtProviderIdentifiction.setText(providerIdentification)
+        binding.txtApplicationName.setText(applicationName)
+        binding.txtSoftwareVersion.setText(softwareVersion)
+        binding.txtCertificationCode.setText(certificationCode)
+    }
+
+
+    private fun saveSettings() {
         try {
-            poiId = txtPOIID!!.text.toString()
-            saleId = txtSalesID!!.text.toString()
-            kek = txtKEK!!.text.toString()
-            providerIdentification = txtProviderIdentifiction!!.text.toString()
-            applicationName = txtApplicationName!!.text.toString()
-            softwareVersion = txtSoftwareVersion!!.text.toString()
-            certificationCode = txtCertificationCode!!.text.toString()
+            poiId = binding.txtPOIID.text.toString()
+            saleId = binding.txtSalesID!!.text.toString()
+            kek = binding.txtKEK.text.toString()
+            providerIdentification = binding.txtProviderIdentifiction.text.toString()
+            applicationName = binding.txtApplicationName.text.toString()
+            softwareVersion = binding.txtSoftwareVersion.text.toString()
+            certificationCode = binding.txtCertificationCode.text.toString()
             Toast.makeText(applicationContext, "Settings Updated", Toast.LENGTH_SHORT).show()
         } catch (e: Exception) {
             Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT).show()
